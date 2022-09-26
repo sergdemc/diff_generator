d890e1d0248ca90dadba001da27e941487f3731b
Sergei Demchenko <hb13718@gmail.com>
2022-09-26 16:54:02 +0300

    Delete loader.py

--- gendiff/loader.py
+++
@@ -1,7 +0,0 @@
-import os
-from gendiff.parser import parse_file
-
-
-def load_file(pathfile: str) -> dict:
-    extension = os.path.splitext(pathfile)[1].lstrip('.')
-    return parse_file(open(pathfile), extension)
